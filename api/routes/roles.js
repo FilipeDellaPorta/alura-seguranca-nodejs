5e36aca42f22f73470c4893963b7f9fd0cec450b
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2024-10-03 18:35:18 -0300

    testado com sucesso o cadastro de roles

---
+++ api/routes/roles.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const RoleController = require("../controllers/roleController");
+const router = Router();
+
+router
+  .post("/roles", RoleController.cadastrar)
+  .get("/roles")
+  .get("/roles/:id")
+  .delete("/roles/:id")
+  .put("/roles/:id");
+
+module.exports = router;
